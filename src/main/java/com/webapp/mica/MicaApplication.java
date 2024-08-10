a3bcbf389df6398eaba71babad20644f2a65915c
Anirudh@2105 <anirudh2105@gmail.com>
2024-08-10 15:05:00 +0530

    Initial Commit

---
+++ src/main/java/com/webapp/mica/MicaApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.mica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicaApplication.class, args);
+	}
+
+}
